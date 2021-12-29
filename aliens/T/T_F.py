ca3b1c1b3c9d43c1b3fe38c489648d7d8a9b0e67
Levanoz2 <96122839+Levanoz2@users.noreply.github.com>
2021-12-30 03:35:46 +0400

    Add files via upload

---
+++ aliens/T/T_F.py
@@ -0,0 +1,33 @@
+age = 7
+age_2 = 20
+
+age > 5 and age_2 >= 23
+
+age >= 11
+age_2 > 24
+
+age < 5 or age_2 >= 29
+
+age = 21
+if age >= 18:
+  print("You'll be able to purchase this game")
+
+  age = 19
+if age >= 16:
+  print("moi")
+else:
+  print("wai")
+
+  age = 25
+if age < 20:
+  price = 15
+elif age < 15:
+  price = 10
+elif age < 10:
+  price = 5 
+else:
+  price = 23 
+
+print("\mEntry price is $" + str(price) + ".")
+
+
